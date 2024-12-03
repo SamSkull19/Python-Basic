ea1c4ee89c67b46ec3c2b7ce56e3ae74bde9f602
SamSkull19 <sifatsamin19@gmail.com>
2024-12-04 00:54:59 +0600

    Operators

---
+++ Operators.py
@@ -0,0 +1,33 @@
+a = 4
+b = 2
+
+# Addition
+add = a + b
+print("Addition: ", add)  # Adds a and b
+
+# Subtraction
+sub = a - b
+print("Subtraction: ", sub)  # Subtracts b from a
+
+# Multiplication
+mul = a * b
+print("Multiplication: ", mul)  # Multiplies a and b
+
+# Power
+pw = a ** b
+print("Power of", a, ": ", pw)  # Raises a to the power of b
+
+# Division
+div = a / b
+print("Division: ", div)  # Divides a by b (result is a float)
+
+# Integer Division (Quotient)
+quotient = a // b
+print("Quotient: ", quotient)  # Divides a by b and returns the integer quotient
+
+# Modulus
+mod = a % b
+print("Modulus: ", mod)  # Returns the remainder of a divided by b
+
+# String Multiplication
+print(4 * "SamSkull ")  # Repeats the string "SamSkull " 4 times
