bb1ac30702bdb6f9e491e6d48fc302be082fe5cf
SamSkull19 <sifatsamin19@gmail.com>
2024-12-07 23:04:18 +0600

    String Basic

---
+++ String_Basic.py
@@ -0,0 +1,21 @@
+my_name = "My name is Samin"
+print(my_name)
+
+# Sentence with single quotes inside a string
+person_name = "His name 'Max Orian'"
+print(person_name)
+
+# Sentence with double quotes inside a string 
+guy_in_class = 'The guy is going into class "His name is Gregory"'
+print(guy_in_class)
+
+# Multiline message to a variable and printing it
+message = '''
+Hello Max,
+
+I am Samin. I am currently living in Sylhet.
+
+Thank You,
+Let me know about yourself.
+'''
+print(message)
