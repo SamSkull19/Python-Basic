fd7d558a26dbacca4e072595585acf2b7829dd4a
SamSkull19 <sifatsamin19@gmail.com>
2024-12-09 19:00:22 +0600

    While Loop

---
+++ While_Loop.py
@@ -0,0 +1,20 @@
+i = 1
+# Loop to print numbers from 1 to 10
+while i <= 10:  # Continue looping while `i` is less than or equal to 10
+    print(i) 
+    i += 1  # Increment `i` by 1 after each iteration
+
+print("Complete")
+
+i = 1
+# Loop to check and print whether numbers from 1 to 10 are even or odd
+while i <= 10: 
+    if i % 2 == 0:  # Check if `i` is divisible by 2 (even number)
+        print(f"Even Number: {i}")
+
+    else:  # If `i` is not divisible by 2
+        print(f"Odd Number: {i}") 
+    
+    i += 1 
+
+print("Complete")
