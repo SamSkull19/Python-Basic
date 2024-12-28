d1eaa16c3e62852e5a3b688bc3055c0ea89b7bbb
samskull <samskull>
2024-12-29 00:31:28 +0600

    Nested Loop

---
+++ Nested_For_Loop.py
@@ -0,0 +1,10 @@
+# Nested loops to generate and print all pairs of (i, j) where i and j range from 1 to 3
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"({i}, {j})")
+
+# Nested loops to generate and print all pairs of (x, y) where x and y range from 0 to 4
+for x in range(5):
+    for y in range(5):
+        print(f"{x} -> {y}")
+                            
