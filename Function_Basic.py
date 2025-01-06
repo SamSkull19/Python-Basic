4e106e8633f9a4a36b1e2397cd7f2d88e1c05ca7
SamSkull19 <sifatsamin19@gmail.com>
2025-01-06 20:28:44 +0600

    Function Basic

---
+++ Function_Basic.py
@@ -0,0 +1,19 @@
+# Function to display a greeting message
+def show_info():
+    print("Hi there,")  # Prints a welcome message
+    print("Where are you from?")
+
+print("Start")
+show_info()  # Calls the show_info function
+print("End")
+
+# Function to calculate and print the sum of three numbers
+def sum():
+    a = 20  
+    b = 30 
+    c = 50  
+    sum = a + b + c 
+    print(sum)  # Prints the calculated sum
+
+print("Sum : ")
+sum()  # Calls the sum function
