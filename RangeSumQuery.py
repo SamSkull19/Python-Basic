6cb5496d9d5c3d378034529a668ef7a29500083b
sifat-samin <sifatsamin19@gmail.com>
2025-06-18 02:25:56 +0600

    Codeforce Problem Replace MinMax

---
+++ RangeSumQuery.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M
+
+n, q = list(map(int, input().split()))
+a = list(map(int, input().split()))
+
+pref = [0] * (n + 1)
+for i in range(1, n + 1):
+    pref[i] = pref[i - 1] + a[i - 1]
+
+for _ in range(q):
+    l, r = list(map(int, input().split()))
+    ans = pref[r] - pref[l - 1]
+    print(ans)
+
+
+
+
+
